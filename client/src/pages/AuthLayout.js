7fa20a8893e9e15f9b6859c688a5368734e329c7
david <david61330@gmail.com>
2024-01-21 21:26:58 +0200

    client: add login layout

---
+++ client/src/pages/AuthLayout.js
@@ -0,0 +1,36 @@
+import { Button, Grid, Stack, Typography } from '@mui/material';
+import { purple } from '@mui/material/colors';
+import React from 'react';
+
+const AuthLayout = ({ children, header, bottomButton }) => {
+  return (
+    <Grid container justifyContent="center">
+      <Grid container maxWidth="md" my={4} sx={{ p: 2 }}>
+        <Grid
+          container
+          item
+          xs={12}
+          sm={7}
+          md={6}
+          sx={{ p: 2, border: 1, borderColor: 'primary.main' }}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography marginY={2} variant="h4" textAlign="center">
+            {header}
+          </Typography>
+          {children}
+          <Stack>
+            <Button href={bottomButton?.target || '#'} style={{ Text }} sx={{ paddingX: 3, marginTop: 1 }}>
+              {bottomButton?.text || 'Buttom Button'}
+            </Button>
+          </Stack>
+        </Grid>
+        <Grid item sm={5} md={6} sx={{ bgcolor: purple[500] }}></Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AuthLayout;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Button, Grid, Typography, Stack, TextField, Divider, Link } from '@mui/material';
+import { Button, Grid, Stack, TextField, Divider, Link } from '@mui/material';
 import GoogleIcon from '@mui/icons-material/Google';
-import { purple } from '@mui/material/colors';
 import { isValidEmail, isValidPassword } from '../helpers/validation';
+import AuthLayout from './AuthLayout';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -16,67 +16,45 @@ const Login = () => {
   };
 
   return (
-    <Grid container justifyContent="center">
-      <Grid container maxWidth="md" my={4} sx={{ p: 2 }}>
-        <Grid
-          container
-          item
-          xs={12}
-          sm={7}
-          md={6}
-          sx={{ p: 2, border: 1, borderColor: 'primary.main' }}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Typography marginY={2} variant="h4" textAlign="center">
-            Login Form
-          </Typography>
-          <Button variant="outlined" sx={{ borderRadius: 8, paddingX: 4 }} startIcon={<GoogleIcon />}>
-            sign in with google
-          </Button>
-          <Stack minWidth="100%" gap={2} sx={{ marginTop: 2 }}>
-            <Divider>or</Divider>
-            <form onSubmit={handleSubmit}>
-              <Stack gap={2}>
-                <TextField
-                  label="Email Address"
-                  type="email"
-                  required
-                  error={!isValidEmail(email)}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                ></TextField>
-                <TextField
-                  label="Password"
-                  type="password"
-                  required
-                  error={!isValidPassword(password)}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                ></TextField>
-                <Grid container direction="row" justifyContent="end">
-                  <Grid item>
-                    <Link variant="body1" href="#" underline="hover">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                </Grid>
-                <Button type="submit" variant="contained" size="large">
-                  sign in
-                </Button>
-              </Stack>
-            </form>
-            <Divider>or</Divider>
-
-            <Stack>
-              <Button style={{ Text }}>Create an acoount</Button>
-            </Stack>
+    <AuthLayout header="Login Form" bottomButton={{ text: 'Register', target: '#' }}>
+      <Button variant="outlined" sx={{ borderRadius: 8, paddingX: 4 }} startIcon={<GoogleIcon />}>
+        sign in with google
+      </Button>
+      <Stack minWidth="100%" gap={2} sx={{ marginTop: 2 }}>
+        <Divider>or</Divider>
+        <form onSubmit={handleSubmit}>
+          <Stack gap={2}>
+            <TextField
+              label="Email Address"
+              type="email"
+              required
+              error={!isValidEmail(email)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></TextField>
+            <TextField
+              label="Password"
+              type="password"
+              required
+              error={!isValidPassword(password)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></TextField>
+            <Grid container direction="row" justifyContent="end">
+              <Grid item>
+                <Link variant="body1" href="#" underline="hover">
+                  Forgot password?
+                </Link>
+              </Grid>
+            </Grid>
+            <Button type="submit" variant="contained" size="large">
+              sign in
+            </Button>
           </Stack>
-        </Grid>
-        <Grid item sm={5} md={6} sx={{ bgcolor: purple[500] }}></Grid>
-      </Grid>
-    </Grid>
+        </form>
+        <Divider>or</Divider>
+      </Stack>
+    </AuthLayout>
   );
 };
 
