99bb1ae7b03750473e683c791adfd9568ccfe91c
david <david61330@gmail.com>
2024-01-21 21:07:29 +0200

    client: Create login form

---
+++ client/src/helpers/validation.js
@@ -0,0 +1,22 @@
+export const isValidEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+export const isValidPassword = (password) => {
+  let strength = 0;
+  if (password.match(/[a-z]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[A-Z]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[0-9]+/)) {
+    strength += 1;
+  }
+
+  return password?.length >= 8 && strength >= 3;
+};
