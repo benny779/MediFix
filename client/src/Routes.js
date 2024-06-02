6afab07a263a997a45134781783b4c2d4b5e003e
david <david61330@gmail.com>
2024-06-03 00:21:45 +0300

    update authorization rout

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -8,11 +8,13 @@ import { tokenExistsAndNotExpired } from './features/authentication';
 console.log(tokenExistsAndNotExpired());
 
 export const router = createBrowserRouter([
+
   { path: '/', element: tokenExistsAndNotExpired() ? <ServiceCalls /> : <Login /> },
   { path: '/ServiceCalls', element: tokenExistsAndNotExpired() ? <ServiceCalls /> : <Login /> },
-  { path: '/login', element: <Login /> },
+  { path: '/login', element: tokenExistsAndNotExpired() ? <ServiceCalls /> : <Login /> },
   { path: '/register', element: <Register /> },
   { path: '/createServiceCall', element: tokenExistsAndNotExpired() ? <CreateServiceCallForm /> : <Login /> },
+
 ]);
 
 export default router;
