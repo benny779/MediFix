7e05e79ab9155806c2a5ba02dce9ac9add20b7b3
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-06-10 18:35:16 +0300

    Improve App Router readability

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ServiceCalls from './pages/user/ServiceCalls';
@@ -8,18 +8,25 @@ import PrivateRoutes from './PrivateRoutes';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route element={<PrivateRoutes />}>
-            <Route element={<ServiceCalls />} path="/" exact />
-            <Route element={<CreateServiceCallForm />} path="/createServiceCall" exact />
+    <BrowserRouter>
+      <Routes>
+        {/* public routes */}
+        <Route element={<Login />} path='/login' />
+        <Route element={<Register />} path='/register' />
+
+        {/* private routes */}
+        <Route element={<PrivateRoutes />}>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/serviceCalls'>
+            <Route index element={<ServiceCalls />} exact />
+            <Route path='new' element={<CreateServiceCallForm />} exact />
           </Route>
-          <Route element={<Login />} path="/login" />
-          <Route element={<Register />} path="/register" />
-        </Routes>
-      </Router>
-    </>
+        </Route>
+
+        {/* catch all */}
+        <Route path='*' element={<h1>Missing</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
