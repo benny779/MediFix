45fbb0f20b553a8060f2fd34e7f8d7b3e3e1eb18
david <david61330@gmail.com>
2024-07-21 20:06:35 +0300

    fix navigate from register file

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -24,7 +24,7 @@ const RegisterForm = () => {
 
   const navigate = useNavigate();
   const location = useLocation();
-  const from = location.state?.from?.pathname || '/';
+  const from = location.state?.from?.pathname || '/login';
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
