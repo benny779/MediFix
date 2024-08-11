f560da23439060fea766df447f8be3b00d4c9d57
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-08-11 23:09:23 +0300

    add link to home in Forbidden componentr

--- client/src/pages/Forbidden.js
+++ client/src/pages/Forbidden.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Forbidden = () => {
   return (
-    <div>Forbidden</div>
-  )
-}
+    <>
+      <div>Forbidden</div>
+      <a href='/'>Home</a>
+    </>
+  );
+};
 
-export default Forbidden
+export default Forbidden;
