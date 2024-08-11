c7db3a06ede7694f60c3c717a8ea714b47e34cd4
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2024-08-11 10:36:47 +0300

    add getTimeDifference

--- client/src/utils/dateHelper.js
+++ client/src/utils/dateHelper.js
@@ -7,4 +7,20 @@ const formatJsonDateTime = (dateTime) => {
 const formatJsonDateToNiceView = (date) =>
   `${date.substring(8, 10)}-${date.substring(5, 7)}-${date.substring(0, 4)}`;
 
-export { formatJsonDateTime };
+const getTimeDifference = (time) => {
+  const now = new Date();
+  const createdDate = new Date(time);
+  const diffInMilliseconds = now - createdDate;
+  const diffInMinutes = Math.floor(diffInMilliseconds / 60000);
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  const diffInDays = Math.floor(diffInHours / 24);
+
+  if (diffInDays > 0) return `${diffInDays}d`;
+  if (diffInHours > 0) {
+    const remainingMinutes = diffInMinutes % 60;
+    return `${diffInHours}h ${remainingMinutes}m`;
+  }
+  return `${diffInMinutes}m`;
+};
+
+export { formatJsonDateTime, getTimeDifference };
